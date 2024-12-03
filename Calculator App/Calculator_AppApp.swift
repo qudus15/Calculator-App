ffa3aec7e8d7aae86fdc2df5fb876d9961c9e9ab
qudus15 <meet.qudus@gmail.com>
2024-12-03 17:12:48 -0500

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Red Whale inc. on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator AppTests/Calculator_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppTests.swift
+//  Calculator AppTests
+//
+//  Created by Red Whale inc. on 03/12/2024.
+//
+
+import Testing
+@testable import Calculator_App
+
+struct Calculator_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
